530779fb6fbf0b8e1cf0b19f948611e9bac4f6e3
root <root@localhost.localdomain>
2021-10-20 12:48:50 +0200

    Pagina web basica creada

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+ <head>
+  <title>Brute Force</title>
+ </head>
+ <body>
+  <form method="post">
+  <center>
+   <table>
+    <tr>
+     <th>Brute force webpage</th>
+    </tr>
+    <tr>
+     <td>
+      User:
+     </td>
+     <td>
+      <input type="text" name="user">
+     </td>
+    </tr>
+    <tr>
+     <td>
+      Password:
+     </td>
+     <td>
+      <input type="password" name="password">
+     </td>
+    </tr>
+   </table>
+  </center>
+  </form>
+ </body>
+</html>
